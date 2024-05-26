6aa627cb291ca40de98d4a7062f8412fe7a35c6d
Lehung59 <nguyenlehungsc@gmail.com>
2024-05-26 22:59:41 +0700

    turn off authen

---
+++ src/main/java/com/example/secumix/security/config/HttpsToHttpFilter.java
@@ -0,0 +1,32 @@
+package com.example.secumix.security.config;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import javax.servlet.Filter;
+
+@Component
+public class HttpsToHttpFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        String url = httpRequest.getRequestURL().toString();
+        if (url.startsWith("https://")) {
+            url = url.replace("https://", "http://");
+            httpResponse.sendRedirect(url);
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+}
